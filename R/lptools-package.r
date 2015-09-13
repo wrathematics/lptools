ab6867e0b3b665a27b26b313306d79d6dce2161c
wrathematics <wrathematics@gmail.com>
2015-09-13 16:28:41 -0400

    added matdisp

---
+++ R/lptools-package.r
@@ -0,0 +1,10 @@
+#' lptools:  Linear programming utilities
+#' 
+#' TODO
+#' 
+#' @name lptools-package
+#' @docType package
+#' @author Drew Schmidt \email{wrathematics AT gmail.com}
+#' @keywords Package
+NULL
+
